1a810c530f95d0b01d26d8bad0624f836481afd3
EsaipQuentin <92928844+EsaipQuentin@users.noreply.github.com>
2021-10-21 14:49:24 +0200

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,62 @@
+var rec = require("./ressources.json");
+var trans = require("./transport.json")
+
+function consoPC(HA)
+{
+    HA=HA*7;
+    let HO=168-HA;
+    let uec =  (0.3*(200*HA+7*HO)/7)*365/1000 + ((1-0.3)*(200*HA+7*HO)/7)*365/1000;
+    return uec/10;
+}
+
+function consoMoni(Nbmoni,HA)
+{
+    HA=HA*7;
+    let HO=168-HA;
+    let uecMoni =  (0.3*(30*HA+7*HO)/7)*365/1000 + ((1-0.3)*(30*HA+7*HO)/7)*365/1000;
+    uecMoni=uecMoni*Nbmoni;
+    return uecMoni/10;
+}
+
+function transport(km,val) {
+    const transp = trans.find(s=>s.id===val);
+    if(val === 1)//#avion
+    {
+        if(km<1000)
+        {
+            return transp.values.find(s=>s.max===1000).value;
+        }    
+        else if(km<2000)
+        {
+            return transp.values.find(s=>s.max===2000).value;
+        }
+        else if(km<3500)
+        {
+            return transp.values.find(s=>s.max===3500).value;
+        }
+        else{
+            return transp.values.find(s=>s.max===10).value;
+        }
+
+    }
+    else
+    {
+    return transp.values.find(s=>s.value)
+    }
+}
+
+
+function cloud(Go)
+{//1Mo=15gco2
+    let Mo=Go*1000;
+    let kgeco2=Mo*15/1000;
+    return kgeco2;
+}
+function mail(nbmail){
+
+}
+
+
+km=1500;
+let Co2trans = km * transport(km,1);
+console.log(Co2trans);
